b62cb64b68d0e57cf5db11142959d47f05a60139
nellysoriacoa <nellysoriacoa@gmail.com>
2025-11-11 19:23:36 -0400

    Actualizar Nuevo Python File.py

--- Nuevo Python File.py
+++ Nuevo Python File.py
@@ -1,4 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 
+# Datos Tabla 1
 data1 = {
     "t": [0.000, 0.200, 0.400, 0.600, 0.800, 1.000],
     "x": [0.000, 0.951, 0.809, 0.588, 0.309, 0.000],
